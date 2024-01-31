559cb66937d544490556d82e52e7ffc823b43495
bkhb <1787626070@qq.com>
2024-01-31 11:22:45 +0800

    添加自动删除前一天的备份文件

--- pal_server_bakup.sh
+++ pal_server_bakup.sh
@@ -5,6 +5,9 @@ target_dir="/home/steam/Pal/AutoSaved"
 # 设置游戏路径
 game_dir="/home/steam/Steam/steamapps/common/PalServer"
 
+# 保留几个存档
+backup_num=5
+
 # 存档
 auto_save(){
     date=$(date +"%Y-%m-%d")
@@ -14,4 +17,10 @@ auto_save(){
     echo "存档已经备份到 $target_dir"
 }
 
+clear_backup(){
+    # 删除前一天的备份
+    find "$target_dir" -name "Saved_*.tar.gz" -mtime +0 -delete
+}
+
 auto_save
+clear_backup
